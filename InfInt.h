54b10488a69c1d29669786f4ecdb39e1168d30fe
sercantutar <sercan.tutar@gmail.com>
2017-01-17 11:54:39 -0800

    Update InfInt.h

--- InfInt.h
+++ InfInt.h
@@ -2,19 +2,9 @@
  * InfInt - Arbitrary-Precision Integer Arithmetic Library
  * Copyright (C) 2013 Sercan Tutar
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  *
  * USAGE:
